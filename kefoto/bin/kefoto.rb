6f99021c5526a0369b10367d0b1729553ea72f91
Alejandra Cantu <38364676+acantu101@users.noreply.github.com>
2019-10-15 17:23:17 -0500

    Add files via upload

---
+++ kefoto/bin/kefoto.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+require_relative "~/CLI.rb"
+
+CLI.new.call
---
+++ kefoto/lib/kefoto.rb
@@ -0,0 +1,6 @@
+require "httparty"
+require 'open-uri'
+require "nokogiri"
+require "pry"
+
+require_relative "./Kefoto_scraper/CLI.rb"
