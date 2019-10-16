85c9afa38c97c0140195181c59e964d7fb136684
Alejandra Cantu <38364676+acantu101@users.noreply.github.com>
2019-10-15 19:35:37 -0500

    changing name of lib folder

--- kefoto/lib/kefoto.rb
+++ kefoto/lib/kefoto.rb
@@ -1,6 +1,8 @@
-require "httparty"
-require 'open-uri'
+
+
+require_relative "./kefoto_scraper/CLI.rb"
+require "kefoto_scraper/version"
 require "nokogiri"
 require "pry"
-
-require_relative "./Kefoto_scraper/CLI.rb"
+require "httparty"
+require_relative "./kefoto_scraper/CLI"
